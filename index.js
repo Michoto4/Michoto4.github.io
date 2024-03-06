878f770029a41c6624e803a5fc2b0aaad1fb3168
Michoto4 <100424788+Michoto4@users.noreply.github.com>
2024-03-06 22:10:11 +0100

    many changes
    Changed "MICHOTO" text to text that says hello to you and tells you actual date,, time and weather. 
    Added  navbar with few example buttons that take you to temponary pages, i will finish them later.

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const url = document.querySelector(".search-bar");
+const dateDisplay = document.getElementById("text2");
+const now = new Date();
+const year = now.getFullYear();
+const month = now.getMonth();
+const monthDay = now.getDate();
+const weekDay = now.getDay();
+const time = now.getTime();
+
+
+
+// to jest do przycisku od wyszukiwarek
 let menuToggle = document.querySelector('.menuToggle');
 menuToggle.onclick=function(){
   menuToggle.classList.toggle('active')
 }
-
-const url = document.querySelector(".search-bar");
+// ------------------------------------------------------------------------ WYSZUKIWARKA | search popup menu
 
 function googleEngine(){
     url.action = "https://www.google.com/search";
@@ -16,3 +27,172 @@ function ddgEngine(){
 function yahooEngine(){
     url.action = "https://search.yahoo.com/search";
 }
+
+// ------------------------------------------------------------------------ PRZYCISKI NA NAVBARZE | navbar buttons
+
+function portfolio(){
+    window.location.assign("portfolio.html");
+}
+function edit(){
+    window.location.assign("edit.html");
+}
+function utils(){
+    window.location.assign("utils.html");
+}
+function settings(){
+    window.location.assign("settings.html");
+}
+
+
+
+
+
+
+// ------------------------------------------------------------------------ AKTUALNA DATA I POGODA I GODZINA | actual date, weather and time
+
+// data | date
+let dayName;
+let monthName;
+
+switch (weekDay) {
+    case 0:
+        dayName = "Niedziela";
+        break;
+    case 1:
+        dayName = "Poniedziałek";
+        break;
+    case 2:
+        dayName = "Wtorek";
+        break;
+    case 3:
+        dayName = "Środa";
+        break;
+    case 4:
+        dayName = "Czwartek";
+        break;
+    case 5:
+        dayName = "Piątek";
+        break;
+    case 6:
+        dayName = "Sobota";
+        break;
+    default:
+        dayName = "Dzień";
+        break;
+  }
+
+
+switch(month){
+    case 0:
+        monthName = "Stycznia";
+        break;
+    case 1:
+        monthName = "Lutego";
+        break;
+    case 2:
+        monthName = "Marca";
+        break;
+    case 3:
+        monthName = "Kwietnia";
+        break;
+    case 4:
+        monthName = "Maja";
+        break;
+    case 5:
+        monthName = "Czerwca";
+        break;
+    case 6:
+        monthName = "Lipca";
+        break;
+    case 7:
+        monthName = "Śierpnia";
+        break;
+    case 8:
+        monthName = "Września";
+        break;
+    case 9:
+        monthName = "Października";
+        break;
+    case 10:
+        monthName = "Listopada";
+        break;
+    case 11:
+        monthName = "Grudnia";
+        break;
+    default:
+        dayName = "Miesiąc";
+        break;
+}
+// data | date
+
+// POGODA | weather
+getWeatherData();
+
+async function fetchWeatherData(){
+    const apiKey = "20bc1b8e012ca6a9c5d008bfde61c15f";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Gdańsk&appid=${apiKey}`;
+    const response = await fetch(apiUrl);
+
+    if(!response.ok){
+        throw new Error("Something went wrong");
+    }
+    return await response.json();
+}
+async function getWeatherData(){
+    try{
+        const weatherData = await fetchWeatherData();
+        DisplayCurrentWeather(weatherData);
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
+
+function DisplayCurrentWeather(data){
+    const {name: city, main: {temp, humidity}, weather: [{description, id}]} = data;
+    
+    dateDisplay.textContent = `Dziś jest ${dayName}, ${monthDay} ${monthName} ${year} | ${(temp - 273.15).toFixed(1)}°C ${getWeatherEmoji(id)}`;
+    
+}
+
+function getWeatherEmoji(weatherId){
+    switch(true){
+        case (weatherId >= 200 && weatherId < 300):
+            return "⛈";
+        case (weatherId >= 300 && weatherId < 400):
+            return "🌧";
+        case (weatherId >= 500 && weatherId < 600):
+            return "☔";
+        case (weatherId >= 600 && weatherId < 700):
+            return "🌨";
+        case (weatherId >= 700 && weatherId < 800):
+            return "🌫";
+        case (weatherId == 800):
+            return "☀";
+        case (weatherId >= 801 && weatherId < 809):
+            return "🌥";
+        default:
+            return "❓";
+        
+    }
+}
+// POGODA | weather
+
+
+
+// ZEGAR | clock
+function updateClock(){
+    const hours = now.getHours().toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").textContent = timeString;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
+// ZEGAR | clock
+
+
+// ------------------------------------------------------------------------ AKTUALNA DATA I POGODA I GODZINA | actual date, weather and time
