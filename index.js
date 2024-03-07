2aa8adc19e466d925f48a4f6cdf99fb52e2b23ec
Michoto4 <100424788+Michoto4@users.noreply.github.com>
2024-03-07 11:31:06 +0100

    Fixed the clock

--- index.js
+++ index.js
@@ -183,6 +183,7 @@ function getWeatherEmoji(weatherId){
 
 // ZEGAR | clock
 function updateClock(){
+    const now = new Date();
     const hours = now.getHours().toString().padStart(2, 0);
     const minutes = now.getMinutes().toString().padStart(2, 0);
     const seconds = now.getSeconds().toString().padStart(2, 0);
