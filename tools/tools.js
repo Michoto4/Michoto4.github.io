d4a4847651a0d206804ace9fdec61cf390fcb295
Michoto4 <100424788+Michoto4@users.noreply.github.com>
2024-03-20 18:19:07 +0100

    update
    added function to close iFrame

--- tools/tools.js
+++ tools/tools.js
@@ -10,16 +10,24 @@
 
 
 const buttons = document.querySelectorAll(".buttons");
-const iFrame = document.getElementById("iframe-embeded-tool");
+const iFrame = document.getElementById("iFrame-embeded-tool");
+const iFrameEmpty = document.getElementById("iFrame-empty");
+console.log(iFrame.src);
+console.log(iFrameEmpty.src);
 
 buttons.forEach(button => {
     button.addEventListener("click", event => {
-
         if(event.target.classList.contains("fa-home")){
             window.location.assign("../index.html");
         }
         else if(event.target.classList.contains("fa-list")){
-            iFrame.src = "../To-DoApp/todoApp.html";
+
+            if(iFrame.src == iFrameEmpty.src){
+                iFrame.src = "../To-DoApp/todoApp.html";
+            }
+            else{
+                iFrame.src = "";
+            }
 
         }
     
